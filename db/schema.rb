59aef1743b91bef05f2156a86441528f78a582d1
bozzywozzy <abozynski@pm.me>
2022-10-16 03:49:59 -0500

    Created a new model called 'Category' with a name attribute as a string. Deleted all data in the Products table to avoid foreign key issues. Migrated the db to add a foreign key reference from the Product model to a category.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_16_061107) do
+ActiveRecord::Schema.define(version: 2022_10_16_084813) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -19,6 +25,9 @@ ActiveRecord::Schema.define(version: 2022_10_16_061107) do
     t.integer "stock_quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  add_foreign_key "products", "categories"
 end

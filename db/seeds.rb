32bc4ae4308dc7b2d5adfa26a80c1ce98760f45c
alina bozynski <111398717+alinabozynski@users.noreply.github.com>
2023-08-24 15:32:56 -0500

    Update seeds.rb
    changed to updated seed file to include categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
+# require 'faker'
 
-676.times do 
+# 676.times do 
+# 	Product.create(
+# 		title: Faker::Commerce.product_name,
+# 		price: Faker::Commerce.price,
+# 		stock_quantity: Faker::Number.between(from: 0, to: 200)
+# 	)
+# end
+
+require 'csv'
+
+Product.destroy_all
+Category.destroy_all
+	
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product| 
+	category = Category.find_or_create_by(name: product['category'])
+	
 	Product.create(
-		title: Faker::Commerce.product_name,
-		price: Faker::Commerce.price,
-		stock_quantity: Faker::Number.between(from: 0, to: 200)
+	category: category,
+	title: product['name'],
+	description: product['description'],
+	price: product['price'],
+	stock_quantity: product['stock quantity']
 	)
 end
